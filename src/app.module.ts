aba3aa950bfc30ce68c09d2ec84d12ac6cdf29d9
Harshil185 <vkabd718@gmail.com>
2025-11-28 19:17:42 +0530

    render index file in controler

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './chat/schemas/user.schema';
 import { Message, MessageSchema } from './chat/schemas/message.schema';
 import { ChatService } from './chat/chat.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 require('dotenv').config();
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../'),
+      serveRoot: '/',
+    }),
     MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ng7eftm.mongodb.net/chat`),
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
